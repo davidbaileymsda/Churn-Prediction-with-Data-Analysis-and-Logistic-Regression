fbc116370a1d1ad8fee01853ddb9bcfe052656fa
davidthedane <davidthedane@gmail.com>
2021-04-10 18:52:28 -0600

    Update Analysis & Practice During Study.R

--- Analysis & Practice During Study.R
+++ Analysis & Practice During Study.R
@@ -2,15 +2,15 @@
 pacman::p_load(pacman, dplyr, GGally, ggplot2, ggthemes, ggvis, httr, lubridate, plotly, rio, rmarkdown, shiny, stringr, tidyr)
 
 # Set the Working Directory for the session
-setwd("C:/Users/david/Desktop/Data Mining & Analytics II Performance Assessment")
+setwd("C:/Users/david/Google Drive/Data/GitHub/Data-Mining-Project-with-R")
 
-# Load the Telco Customer Churn dataset
+# Load the Telco Customer Churn data set
 customer.churn <- read.csv("WA_Fn-UseC_-Telco-Customer-Churn.csv",header = TRUE)
 
 # View the structure of the data frame
 str(customer.churn)
 
-# View summary statistics for the uncleaned dataset and check for missing values
+# View summary statistics for the uncleaned data set and check for missing values
 summary(customer.churn)
 
 # Create new data.frame for 11 records with missing TotalCharges value
@@ -29,7 +29,7 @@ missing.values <- data.frame(subset(customer.churn,is.na(TotalCharges)))
 ### Clean Up ###
 
 # Clear Packages
-detach("package:ggplot2", unload = TRUE)
+p_unload(all)
 
 # Clear Plots, if there are any
 dev.off()
